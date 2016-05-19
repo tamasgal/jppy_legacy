ca9cc4f7b6bba4f76c6beae76c270a9e9e139ff8
Tamas Gal <tgal@km3net.de>
2016-05-19 23:40:01 +0200

    Cleans up the first minimal prototype

--- readout.cc
+++ readout.cc
@@ -1,38 +1,39 @@
-#include <string>
-#include <iostream>
-#include <iomanip>
-#include <limits>
-
+// #include <vector>
 #include "JDAQ/JDAQEvent.hh"
-
 #include "JSupport/JMultipleFileScanner.hh"
 
-#include "Jeep/JParser.hh"
-#include "Jeep/JMessage.hh"
+struct Hit {
+    int tot, time;
+};
 
-
-/**
- * \file
- * Auxiliary program to analyse KM3NETDAQ::JDAQEvent.
- */
 int main(int argc, char **argv)
 {
-  using namespace std;
-  using namespace JSUPPORT;
-  using namespace KM3NETDAQ;
+    typedef KM3NETDAQ::JDAQTriggeredHit JHit_t;
 
-  JFileScanner<JDAQEvent> scan;
-  scan.open(argv[1]);
+    JSUPPORT::JFileScanner<KM3NETDAQ::JDAQEvent> scan;
+    scan.open(argv[1]);
 
-  while (scan.hasNext()) {
+    int event_id = 0;
 
-    JDAQEvent* event = scan.next();
+    while (scan.hasNext())
     {
-      typedef JDAQTriggeredHit JHit_t;
 
-      for (JDAQEvent::const_iterator<JHit_t> hit = event->begin<JHit_t>(); hit != event->end<JHit_t>(); ++hit) {
-         std::cout << "Hit  tot:" << (int)hit->getToT() << "ns   time: " << hit->getT() << std::endl;
-      }
+        KM3NETDAQ::JDAQEvent* event = scan.next();
+        {
+            std::cout << "Run: " << event->getRunNumber()
+                      << "  Event: " << event_id++
+                      << "  Frame Index: " << event->getFrameIndex()
+                      << std::endl;
+
+            for (KM3NETDAQ::JDAQEvent::const_iterator<JHit_t>
+                    hit = event->begin<JHit_t>();
+                 hit != event->end<JHit_t>();
+                 ++hit)
+            {
+                struct Hit AHit;
+                AHit.tot = (int)hit->getToT();
+                AHit.time = (int)hit->getT();
+            }
+        }
     }
-  }
 }
