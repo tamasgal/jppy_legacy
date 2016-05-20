4cb6cead12827195498724214f7b6dd2362ec81a
Tamas Gal <tgal@km3net.de>
2016-05-20 09:40:47 +0200

    Ignore swap files and other build stuff

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+make clean
+rm libreadout.so
+make
+
+g++ -shared -Wl \
+    -L${JPP_LIB} \
+    -L${ANTRS_LIB} \
+    `/usr/local/root/v5.34.23//bin/root-config --libs` \
+    -lKM3NeTDAQROOT \
+    -llang \
+    -o libreadout.so  readout.o
