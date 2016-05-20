16cbdaa18e2067bacca83329c458b5a80c0054ef
Tamas Gal <tgal@km3net.de>
2016-05-20 10:20:12 +0200

    Argument passing from python to C++ now working

--- readout.cc
+++ readout.cc
@@ -12,8 +12,9 @@ struct Hit {
 
 class JDAQEventReader{
     public:
-        JDAQEventReader() {
-            file_scanner.open("test.root");
+        JDAQEventReader(char* filename) {
+            std::cout << "Filename in c++: " << filename << std::endl;
+            file_scanner.open(filename);
             event_id = 0;
         }
         int get_next_frame_index() {
@@ -21,39 +22,11 @@ class JDAQEventReader{
             KM3NETDAQ::JDAQEvent* event = file_scanner.next();
             return event->getFrameIndex();
         };
-    private:
-        int event_id;
-        JSUPPORT::JFileScanner<KM3NETDAQ::JDAQEvent> file_scanner;
-};
-
-extern "C" {
-    JDAQEventReader* JDAQEventReader_new(){ return new JDAQEventReader(); }
-    int JDAQEventReader_get_next_frame_index(JDAQEventReader* r){
-        return r->get_next_frame_index();
-    }
-}
-
-
-
-int main(int argc, char **argv)
-{
-    typedef KM3NETDAQ::JDAQTriggeredHit JHit_t;
-
-    JSUPPORT::JFileScanner<KM3NETDAQ::JDAQEvent> scan;
-    scan.open(argv[1]);
-
-    int event_id = 0;
-
-    while (scan.hasNext())
-    {
-
-        KM3NETDAQ::JDAQEvent* event = scan.next();
-        {
-            std::cout << "Run: " << event->getRunNumber()
-                      << "  Event: " << event_id++
-                      << "  Frame Index: " << event->getFrameIndex()
-                      << std::endl;
-
+        bool has_next() {
+            return file_scanner.hasNext();
+        }
+        void hits_dummy() {
+            /*
             for (KM3NETDAQ::JDAQEvent::const_iterator<JHit_t>
                     hit = event->begin<JHit_t>();
                  hit != event->end<JHit_t>();
@@ -63,6 +36,26 @@ int main(int argc, char **argv)
                 AHit.tot = (int)hit->getToT();
                 AHit.time = (int)hit->getT();
             }
+            */
         }
+    private:
+        int event_id;
+        JSUPPORT::JFileScanner<KM3NETDAQ::JDAQEvent> file_scanner;
+};
+
+extern "C" {
+    JDAQEventReader* JDAQEventReader_new(char* filename){ return new JDAQEventReader(filename); }
+    int JDAQEventReader_get_next_frame_index(JDAQEventReader* r){
+        return r->get_next_frame_index();
     }
+    bool JDAQEventReader_has_next(JDAQEventReader* r){
+        return r->has_next();
+    }
+}
+
+
+
+int main(int argc, char **argv)
+{
+    return 0;
 }
--- readout.py
+++ readout.py
@@ -1,15 +1,28 @@
-from ctypes import cdll
+from ctypes import cdll, c_char_p
+import sys
+
 lib = cdll.LoadLibrary('./libreadout.so')
 
+lib.JDAQEventReader_new.argtypes = [c_char_p]
+
 class JDAQEventReader(object):
-    def __init__(self):
-        self.obj = lib.JDAQEventReader_new()
+    def __init__(self, filename):
+        print("Filename: {0}".format(filename))
+        self.obj = lib.JDAQEventReader_new(filename)
 
     def get_next_frame_index(self):
         return lib.JDAQEventReader_get_next_frame_index(self.obj)
 
+    def has_next(self):
+        return lib.JDAQEventReader_has_next(self.obj)
 
-reader = JDAQEventReader()
-for i in range(4):
-    idx = reader.get_next_frame_index()
-    print("Getting event #{0}\n  FrameIndex: {1}".format(i, idx))
+    def event_generator(self):
+        while self.has_next():
+            yield self.get_next_frame_index()
+
+
+reader = JDAQEventReader(sys.argv[1])
+
+event_generator = reader.event_generator()
+for event in event_generator:
+    print(event)
