11b8e2ba439b617c75188b802b1add02f48c0bd3
Tamas Gal <tgal@km3net.de>
2016-05-23 22:43:52 +0200

    Fixes include paths

--- nomake/setup.py
+++ nomake/setup.py
@@ -1,11 +1,18 @@
+import os
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
+import subprocess
+
+ROOT_INC = subprocess.Popen(["root-config", "--incdir"],
+                            stdout=subprocess.PIPE).communicate()[0].strip()
+JPP_INC = os.environ['JPP_INC']
 
 extensions = [
     Extension("jppp", ["jppp.pyx"],
-              include_dirs=["/opt/jpp/software"],
+              include_dirs=[JPP_INC, ROOT_INC, ],
               #libraries = [...],
               #library_dirs = [...]
              ),
