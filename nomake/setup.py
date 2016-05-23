c28ff8364f15c5b5b77a0c7d2aa518e4df7e520f
Moritz Lotze <lotzemoritz@gmail.com>
2016-05-23 22:17:13 +0200

    use distutils extensions

--- nomake/setup.py
+++ nomake/setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize(
-           "jppp.pyx",                 # our Cython source
-           sources=["readout.cpp"],  # additional source file(s)
-           language="c++",             # generate C++ code
-           extra_compile_args=["-c", "-fPIC", "-I$(root-config --incdir)",
-                               "$(python-config --includes)", "-I/sps/km3net/users/tgal/apps/jpp/trunk/software"],
-      ))
+
+extensions = [
+    Extension("jppp", ["jppp.pyx"],
+              include_dirs=["/opt/jpp/software"],
+              #libraries = [...],
+              #library_dirs = [...]
+             ),
+]
+setup(ext_modules = cythonize(extensions))
