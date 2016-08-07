7a7fe102b6513dc184cd7f102ebb7faec9aa5821
Moritz Lotze <lotzemoritz@gmail.com>
2016-08-07 22:19:06 +0200

    use actual getX api

--- jppy.pyx
+++ jppy.pyx
@@ -38,8 +38,13 @@ cdef extern from "JMCEventReader.h" namespace "jppy":
         void retrieveNextEvent()
         int getNumberOfMCHits()
         int getNumberOfMCTracks()
-        void getMCHits()
-        void getMCTracks()
+        void getMCHits(int* types, float* lengths, int* pmt_ids, 
+                       float* pos_xs, float* pos_ys, float* pos_zs,
+                       float* dir_xs, float* dir_ys, float* dir_zs,
+                       float* energies, float* times,)
+        void getMCTracks( int* types, int* origins, float* pure_dts, 
+                         float* pure_npes, int* idents, int* pmt_ids, 
+                         float* dts, float* npes)
 
 
 #cdef extern from "JDAQSummarysliceReader.h" namespace "jppy":
