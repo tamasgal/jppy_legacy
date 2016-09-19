069c3fec2c1184983ce2363c7d41fb4ea5f9931c
Tamas Gal <himself@tamasgal.com>
2016-09-19 23:50:46 +0200

    Bumps version number

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ extensions = [
 ]
 
 setup(name='jppy',
-      version='1.2.2',
+      version='1.2.3',
       url='http://git.km3net.de/tgal/jppy.git',
       description='Python bindings for JPP',
       author='Tamas Gal',
