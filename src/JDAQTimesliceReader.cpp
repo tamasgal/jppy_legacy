598d73717524f57ffd9741cb51d939f917dfdcc2
Tamas Gal <tgal@km3net.de>
2016-09-20 10:40:20 +0200

    Finished iterator interface

--- src/JDAQTimesliceReader.cpp
+++ src/JDAQTimesliceReader.cpp
@@ -38,28 +38,15 @@ namespace jppy {
     }
 
     void JDAQTimesliceReader::getHits() {
-        int n_superframe = 0;
-        int n_hit = 0;
-        superframe_it = timeslice->begin();
-
-        for( JDAQTimeslice::const_iterator superframe = timeslice->begin();
-             superframe != timeslice->end();
-             superframe++ ) {
-
-            std::cout << "Superframe #" << n_superframe;
-            for( JDAQSuperFrame::const_iterator hit=superframe->begin();
-                 hit!=superframe->end();
-                 ++hit ) {
-                n_hit += 1;
-                //std::cout << "    DOM: " << superframe->getModuleID();
-                //std::cout << " , PMT: " << int(hit->getPMT());
-                //std::cout << " , time: " << hit->getT();
-                //std::cout << " , tot: " << int(hit->getToT()) << std::endl;
-            }
-            std::cout << " - " << n_hit << " hits" << std::endl;
-            std::cout << superframe->size() << std::endl;
-            n_hit = 0;
-            n_superframe += 1;
+        std::cout << "Superframe" << std::endl;
+        for( JDAQSuperFrame::const_iterator hit=superframe_it->begin();
+             hit!=superframe_it->end();
+             ++hit ) {
+            //n_hit += 1;
+            std::cout << "    DOM: " << superframe_it->getModuleID();
+            std::cout << " , PMT: " << int(hit->getPMT());
+            std::cout << " , time: " << hit->getT();
+            std::cout << " , tot: " << int(hit->getToT()) << std::endl;
         }
     }
 }
