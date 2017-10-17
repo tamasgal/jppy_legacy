136c32e04427c1a8bc2f66f5ee7430b6e1f209b2
Tamas Gal <tgal@km3net.de>
2017-10-17 17:55:57 +0200

    Fix typo

--- src/JDAQSummarysliceReader.cpp
+++ src/JDAQSummarysliceReader.cpp
@@ -94,7 +94,7 @@ namespace jppy {
         return frame_it->testFIFOStatus();
     }
 
-    void getRates(float* rates) {
+    void JDAQSummarysliceReader::getRates(float* rates) {
         for (int channel_id = 0; channel_id != 31; ++channel_id) {
             std::cout << "Channel " << channel_id << " rate: ";
             std::cout << frame_it->getRate(channel_id) << std::endl;
