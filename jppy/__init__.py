4e952670dfe8f1547c5d321839724d0c61baa408
Tamas Gal <tgal@km3net.de>
2017-11-03 10:48:34 +0100

    Skip jfitreader

--- jppy/__init__.py
+++ jppy/__init__.py
@@ -1,4 +1,4 @@
 from jppy.daqeventreader import PyJDAQEventReader
 from jppy.daqsummaryslicereader import PyJDAQSummarysliceReader
 from jppy.daqtimeslicereader import PyJDAQTimesliceReader
-from jppy.jfitreader import PyJFitReader
+# from jppy.jfitreader import PyJFitReader
